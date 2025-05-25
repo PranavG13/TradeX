2d81c1c71efb7b4706573a2c466a847056d7cf37
PavanTeja2005 <98730339+PavanTeja2005@users.noreply.github.com>
2025-05-25 06:08:43 +0000

    Modified End points

--- backend/core/recommend_backtest/backtesting_apiendpoints.py
+++ backend/core/recommend_backtest/backtesting_apiendpoints.py
@@ -15,7 +15,7 @@ def backtesting(request):
         print(f'exception : {e}')
         return Response({"error":"Invalid Data received"}, status=status.HTTP_400_BAD_REQUEST)
     try:
-        metrics, tt, profit, initial_price, buyNhold, first_close = run_script(symbol, interval, code)
-        return Response({metrics, tt, profit, initial_price, buyNhold, first_close}, status=status.HTTP_200_OK)
+        metrics, tt = run_script(symbol, interval, code)
+        return Response({metrics, tt}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error":"Error encountered"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

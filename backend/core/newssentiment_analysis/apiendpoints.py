25bdf228ac1a70248117f05bc2741e335ad5e4e8
Ajmal-taha <117922551+Ajmal-taha@users.noreply.github.com>
2025-06-26 14:50:55 +0530

    add indian stock news

--- backend/core/newssentiment_analysis/apiendpoints.py
+++ backend/core/newssentiment_analysis/apiendpoints.py
@@ -2,6 +2,7 @@ import joblib
 import re
 import numpy as np
 import requests
+from datetime import datetime, timedelta
 from textblob import TextBlob
 from rest_framework.response import Response
 from rest_framework import status
@@ -38,6 +39,9 @@ def preprocess(text):
 
 def get_sentiment_score(text):
     return TextBlob(text).sentiment.polarity
+
+def is_indian_stock(symbol):
+    return symbol.endswith(".NS") or symbol.endswith(".BO")
     
 @api_view(['POST'])
 def news_and_sentiment(request):
@@ -58,24 +62,62 @@ def news_and_sentiment(request):
         data = request.data
         symbol = data.get("symbol")
         limit = data.get("limit", 10)
-        api_key = "sZvNY6tfUphR9fVk3pCMy9aapX7yhz23dOa1UIrp"
+
+        now = datetime.utcnow()
+        one_week_ago = now - timedelta(days=7)
+        from_date = one_week_ago.strftime("%Y-%m-%d")
+        to_date = now.strftime("%Y-%m-%d")
+        published_after = one_week_ago.strftime("%Y-%m-%dT%H:%M")
     except Exception as e:
         print(f'Error in fetch news, {e}')
         return Response({"error": "cannot process request"}, status=status.HTTP_400_BAD_REQUEST)
 
-
-    url = f"https://api.marketaux.com/v1/news/all?symbols={symbol}&limit={limit}&language=en&api_token={api_key}"
-    try:
-        response = requests.get(url)
-        data = response.json()
-        news_data = [{
-            "title": article["title"],
-            "published_at": article["published_at"],
-            "url": article["url"]
-        } for article in data.get("data", [])]
-    except Exception as e:
-        print(f'Error in fetch news, {e}')
-        return Response({"error": "error while processing request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if is_indian_stock(symbol):
+        api_key = "sZvNY6tfUphR9fVk3pCMy9aapX7yhz23dOa1UIrp"
+        url = f"https://api.marketaux.com/v1/news/all"
+        params = {
+            "symbols": symbol,
+            "countries": "in",
+            "filter_entities": "true",
+            "limit": limit,
+            "language": "en",
+            "published_after": published_after,
+            "api_token": api_key
+        }
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            news_data = [{
+                "title": article["title"],
+                "published_at": article["published_at"],
+                "url": article["url"]
+            } for article in data.get("data", [])]
+            # return jsonify({"news": news_data})
+        except Exception as e:
+            print(f'Error in fetch news, {e}')
+            return Response({"error": "error while processing request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        api_key = "d0m99ehr01qkesvjceg0d0m99ehr01qkesvjcegg"
+        url = f"https://finnhub.io/api/v1/company-news"
+        params = {
+            "symbol": symbol,
+            "from": from_date,
+            "to": to_date,
+            "token": api_key
+        }
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            news_data = [{
+                "title": article["headline"],
+                "published_at": article["datetime"],
+                "url": article["url"]
+            } for article in data]
+            # return jsonify({"news": news_data[:limit]})
+        except Exception as e:
+            print(f'Error in fetch news, {e}')
+            return Response({"error": "error while processing request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
 
     headlines = [article["title"] for article in news_data]
     urls = [article["url"] for article in news_data]
